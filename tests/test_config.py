cffdbb45d408bf1658b81146451b62706f9aa3c0
kmthach <kmthach.ai@gmail.com>
2023-02-19 13:31:27 +0700

    add test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,18 @@
 
+import pytest
 
+
+class NotInRangeException(Exception):
+    def __init__(self, message = 'Not In Range'):
+        super().__init__(message)
+        
 def test_generic():
+    a = 10
+    with pytest.raises(NotInRangeException):
+        if a not in range(10, 20):
+            raise NotInRangeException
+        
+def test_equal():
     a = 2
     b = 2
-    assert a == b
+    assert a ==b 
