e98b940894395e6235b8ac2359008a1523145d2b
kmthach <kmthach.ai@gmail.com>
2023-02-21 11:08:45 +0700

    add github actions

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,25 +8,13 @@ class NotInRangeException(Exception):
         
 def test_generic():
     a = 10
-    with pytest.raises(NotInRangeException):
-        if a not in range(10, 20):
-            raise NotInRangeException
+    if a not in range(10, 20):
+        raise NotInRangeException
         
 def test_equal():
     a = 2
     b = 2
     assert a ==b 
     
-    
-def wrapper(i):
-    i()
-    def new_hello(f):
-        f()
-        
-    def hi(f):
-        f()
-
-@wrapper
-def hello():
-    print('Hello')
+ 
 
